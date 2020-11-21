c6e4a2386e326a68465f6c173dd4e193a411b6c1
unknown <ismty0805@gmail.com>
2020-11-21 22:07:11 +0900

    added korean

--- final.py
+++ final.py
@@ -1,12 +1,13 @@
 import cv2, pickle
 import numpy as np
 import tensorflow as tf
+from PIL import ImageFont, ImageDraw, Image
 # from cnn_tf import cnn_model_fn
 import os
 import sqlite3, pyttsx3
 from keras.models import load_model
 from threading import Thread
-
+import time
 engine = pyttsx3.init()
 engine.setProperty('rate', 150)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'	
@@ -38,7 +39,8 @@ def keras_predict(model, image):
 def get_pred_text_from_db(pred_class):
 	# conn = sqlite3.connect("gesture_db.db")
 	# cmd = "SELECT g_name FROM gesture WHERE g_id="+str(pred_class)
-	pred_texts = ["k", "n", "d", "l", "m", "b", "s", "ng", "ja", "cha", "ka", "ta", "pa", "ha", "a", "eo", "o", "u", "eu", "i"]
+	# pred_texts = ["k", "n", "d", "l", "m", "b", "s", "ng", "ja", "cha", "ka", "ta", "pa", "ha", "a", "eo", "o", "u", "eu", "i"]
+	pred_texts = ["ㄱ", "ㄴ", "ㄷ", "ㄹ", "ㅁ", "ㅂ", "ㅅ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ", "ㅏ", "ㅓ", "ㅗ", "ㅜ", "ㅡ", "ㅣ"]
 	return pred_texts[pred_class]
 	
 	# cursor = conn.execute(cmd)
@@ -57,34 +59,6 @@ def get_pred_from_contour(contour, thresh):
 	if pred_probab*100 > 70:
 		text = get_pred_text_from_db(pred_class)
 	return text
-
-def get_operator(pred_text):
-	try:
-		pred_text = int(pred_text)
-	except:
-		return ""
-	operator = ""
-	if pred_text == 1:
-		operator = "+"
-	elif pred_text == 2:
-		operator = "-"
-	elif pred_text == 3:
-		operator = "*"
-	elif pred_text == 4:
-		operator = "/"
-	elif pred_text == 5:
-		operator = "%"
-	elif pred_text == 6:
-		operator = "**"
-	elif pred_text == 7:
-		operator = ">>"
-	elif pred_text == 8:
-		operator = "<<"
-	elif pred_text == 9:
-		operator = "&"
-	elif pred_text == 0:
-		operator = "|"
-	return operator
 
 hist = get_hand_hist()
 x, y, w, h = 300, 100, 300, 300
@@ -113,136 +87,13 @@ def say_text(text):
 	engine.say(text)
 	engine.runAndWait()
 
-def calculator_mode(cam):
-	global is_voice_on
-	flag = {"first": False, "operator": False, "second": False, "clear": False}
-	count_same_frames = 0
-	first, operator, second = "", "", ""
-	pred_text = ""
-	calc_text = ""
-	info = "Enter first number"
-	Thread(target=say_text, args=(info,)).start()
-	count_clear_frames = 0
-	while True:
-		img = cam.read()[1]
-		img = cv2.resize(img, (640, 480))
-		img, contours, thresh = get_img_contour_thresh(img)
-		old_pred_text = pred_text
-		if len(contours) > 0:
-			contour = max(contours, key = cv2.contourArea)
-			if cv2.contourArea(contour) > 10000:
-				pred_text = get_pred_from_contour(contour, thresh)
-				if old_pred_text == pred_text:
-					count_same_frames += 1
-				else:
-					count_same_frames = 0
-
-				if pred_text == "C":
-					if count_same_frames > 5:
-						count_same_frames = 0
-						first, second, operator, pred_text, calc_text = '', '', '', '', ''
-						flag['first'], flag['operator'], flag['second'], flag['clear'] = False, False, False, False
-						info = "Enter first number"
-						Thread(target=say_text, args=(info,)).start()
-
-				elif pred_text == "Best of Luck " and count_same_frames > 15:
-					count_same_frames = 0
-					if flag['clear']:
-						first, second, operator, pred_text, calc_text = '', '', '', '', ''
-						flag['first'], flag['operator'], flag['second'], flag['clear'] = False, False, False, False
-						info = "Enter first number"
-						Thread(target=say_text, args=(info,)).start()
-					elif second != '':
-						flag['second'] = True
-						info = "Clear screen"
-						#Thread(target=say_text, args=(info,)).start()
-						second = ''
-						flag['clear'] = True
-						try:
-							calc_text += "= "+str(eval(calc_text))
-						except:
-							calc_text = "Invalid operation"
-						if is_voice_on:
-							speech = calc_text
-							speech = speech.replace('-', ' minus ')
-							speech = speech.replace('/', ' divided by ')
-							speech = speech.replace('**', ' raised to the power ')
-							speech = speech.replace('*', ' multiplied by ')
-							speech = speech.replace('%', ' mod ')
-							speech = speech.replace('>>', ' bitwise right shift ')
-							speech = speech.replace('<<', ' bitwise leftt shift ')
-							speech = speech.replace('&', ' bitwise and ')
-							speech = speech.replace('|', ' bitwise or ')
-							Thread(target=say_text, args=(speech,)).start()
-					elif first != '':
-						flag['first'] = True
-						info = "Enter operator"
-						Thread(target=say_text, args=(info,)).start()
-						first = ''
-
-				elif pred_text != "Best of Luck " and pred_text.isnumeric():
-					if flag['first'] == False:
-						if count_same_frames > 15:
-							count_same_frames = 0
-							Thread(target=say_text, args=(pred_text,)).start()
-							first += pred_text
-							calc_text += pred_text
-					elif flag['operator'] == False:
-						operator = get_operator(pred_text)
-						if count_same_frames > 15:
-							count_same_frames = 0
-							flag['operator'] = True
-							calc_text += operator
-							info = "Enter second number"
-							Thread(target=say_text, args=(info,)).start()
-							operator = ''
-					elif flag['second'] == False:
-						if count_same_frames > 15:
-							Thread(target=say_text, args=(pred_text,)).start()
-							second += pred_text
-							calc_text += pred_text
-							count_same_frames = 0	
-
-		if count_clear_frames == 30:
-			first, second, operator, pred_text, calc_text = '', '', '', '', ''
-			flag['first'], flag['operator'], flag['second'], flag['clear'] = False, False, False, False
-			info = "Enter first number"
-			Thread(target=say_text, args=(info,)).start()
-			count_clear_frames = 0
-
-		blackboard = np.zeros((480, 640, 3), dtype=np.uint8)
-		cv2.putText(blackboard, "Calculator Mode", (100, 50), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255, 0,0))
-		cv2.putText(blackboard, "Predicted text- " + pred_text, (30, 100), cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 255, 0))
-		cv2.putText(blackboard, "Operator " + operator, (30, 140), cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 255, 127))
-		cv2.putText(blackboard, calc_text, (30, 240), cv2.FONT_HERSHEY_TRIPLEX, 2, (255, 255, 255))
-		cv2.putText(blackboard, info, (30, 440), cv2.FONT_HERSHEY_TRIPLEX, 1, (0, 255, 255) )
-		if is_voice_on:
-			cv2.putText(blackboard, " ", (450, 440), cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 127, 0))
-		else:
-			cv2.putText(blackboard, " ", (450, 440), cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 127, 0))
-		cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
-		res = np.hstack((img, blackboard))
-		cv2.imshow("Recognizing gesture", res)
-		cv2.imshow("thresh", thresh)
-		keypress = cv2.waitKey(1)
-		if keypress == ord('q') or keypress == ord('t'):
-			break
-		if keypress == ord('v') and is_voice_on:
-			is_voice_on = False
-		elif keypress == ord('v') and not is_voice_on:
-			is_voice_on = True
-
-	if keypress == ord('t'):
-		return 1
-	else:
-		return 0
-
 def text_mode(cam):
 	global is_voice_on
 	text = ""
 	word = ""
 	count_same_frame = 0
 	while True:
+		print("a", time.time())
 		img = cam.read()[1]
 		img = cv2.resize(img, (640, 480))
 		img, contours, thresh = get_img_contour_thresh(img)
@@ -271,6 +122,7 @@ def text_mode(cam):
 					#print('yolo')
 					#say_text(text)
 					Thread(target=say_text, args=(word, )).start()
+				print("erase")
 				text = ""
 				word = ""
 		else:
@@ -280,10 +132,25 @@ def text_mode(cam):
 				Thread(target=say_text, args=(word, )).start()
 			text = ""
 			word = ""
+		print("b", time.time())
 		blackboard = np.zeros((480, 640, 3), dtype=np.uint8)
-		cv2.putText(blackboard, " ", (180, 50), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255, 0,0))
-		cv2.putText(blackboard, "Predicted text- " + text, (30, 100), cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 255, 0))
-		cv2.putText(blackboard, word, (30, 240), cv2.FONT_HERSHEY_TRIPLEX, 2, (255, 255, 255))
+		b,g,r,a = 255,255,255,0
+		img_pil = Image.fromarray(blackboard)
+		draw = ImageDraw.Draw(img_pil)
+		draw.text((180,50), " ", font =ImageFont.truetype('malgun.ttf', 36), fill=(255, 0,0, 0))
+		draw.text((30, 100), "Predicted text-" + text, font = ImageFont.truetype('malgun.ttf', 36), fill = (255, 255, 0, 0))
+		draw.text((30, 240), word, font=ImageFont.truetype('malgun.ttf', 36), fill=(b,g,r,a))
+		blackboard = np.array(img_pil)
+		print("c", time.time())
+		# cv2.putText(blackboard, " ", (180, 50), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255, 0,0))
+		# cv2.putText(blackboard, "Predicted text-" + text, (30, 100), cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 255, 0))
+		# img = np.zeros((200,400,3),np.uint8)
+		
+		# fontpath = "fonts/gulim.ttc"
+		# font = ImageFont.truetype(fontpath, 20)
+		
+		# cv2.putText(blackboard, word, (30, 240), cv2.FONT_HERSHEY_TRIPLEX, 2, (255, 255, 255))
+
 		if is_voice_on:
 			cv2.putText(blackboard, " ", (450, 440), cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 127, 0))
 		else:
@@ -299,7 +166,7 @@ def text_mode(cam):
 			is_voice_on = False
 		elif keypress == ord('v') and not is_voice_on:
 			is_voice_on = True
-
+		print("d", time.time())
 	if keypress == ord('c'):
 		return 2
 	else:
@@ -316,8 +183,6 @@ def recognize():
 	while True:
 		if keypress == 1:
 			keypress = text_mode(cam)
-		elif keypress == 2:
-			keypress = calculator_mode(cam)
 		else:
 			break
 
--- set_hand_histogram.py
+++ set_hand_histogram.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 import pickle
-
+import time
 def build_squares(img):
 	x, y, w, h = 420, 140, 10, 10
 	d = 10
@@ -39,7 +39,32 @@ def get_hand_hist():
 		hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 		
 		keypress = cv2.waitKey(1)
-		if keypress == ord('c'):		
+		if keypress == ord('c'):	
+			# delay=3
+			# close_time=time.time()+delay
+			# while True:
+			# 	img = cam.read()[1]
+			# 	img = cv2.flip(img, 1)
+			# 	img = cv2.resize(img, (640, 480))
+			# 	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+			# 	cv2.imshow("Set hand histogram", img)
+			# 	if flagPressedC:	
+			# 		dst = cv2.calcBackProject([hsv], [0, 1], hist, [0, 180, 0, 256], 1)
+			# 		dst1 = dst.copy()
+			# 		disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(10,10))
+			# 		cv2.filter2D(dst,-1,disc,dst)
+			# 		blur = cv2.GaussianBlur(dst, (11,11), 0)
+			# 		blur = cv2.GaussianBlur(dst, (11,11), 0)
+			# 		blur = cv2.GaussianBlur(dst, (11,11), 0)
+			# 		blur = cv2.medianBlur(blur, 15)
+			# 		ret,thresh = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+			# 		thresh = cv2.merge((thresh,thresh,thresh))
+			# 		#cv2.imshow("res", res)
+			# 		cv2.imshow("Thresh", thresh)
+			# 	if not flagPressedS:
+			# 		imgCrop = build_squares(img)
+			# 	if time.time()>close_time:
+			# 		break
 			hsvCrop = cv2.cvtColor(imgCrop, cv2.COLOR_BGR2HSV)
 			flagPressedC = True
 			hist = cv2.calcHist([hsvCrop], [0, 1], None, [180, 256], [0, 180, 0, 256])
