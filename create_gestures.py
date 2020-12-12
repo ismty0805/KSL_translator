8c40a499c634d3760ea4a03bb6c9c695b61e07ba
ismty0805 <48056196+ismty0805@users.noreply.github.com>
2020-12-12 22:01:30 +0900

    Update create_gestures.py

--- create_gestures.py
+++ create_gestures.py
@@ -4,7 +4,7 @@ import pickle, os, sqlite3, random
 
 image_x, image_y = 50, 50
 
-## hist 정보를 로드하고 반환
+## hist 정보를 로드하고 리턴
 def get_hand_hist():
    with open("hist", "rb") as f:
       hist = pickle.load(f)
@@ -21,12 +21,12 @@ def init_create_folder_database():
       conn.execute(create_table_cmd)
       conn.commit()
 
-## folder_name 폴더를 생성한다(없으면)
+## create the folder_name directory and database if not exist
 def create_folder(folder_name):
    if not os.path.exists(folder_name):
       os.mkdir(folder_name)
 
-## g_id 와 g_name을 database에 저장한다.
+## g_id 와 g_name을 database에 저장
 def store_in_db(g_id, g_name):
    conn = sqlite3.connect("gesture_db.db")
    cmd = "INSERT INTO gesture (g_id, g_name) VALUES (%s, \'%s\')" % (g_id, g_name)
@@ -41,7 +41,9 @@ def store_in_db(g_id, g_name):
          print("Doing nothing...")
          return
    conn.commit()
-   
+
+
+## image 촬영 및 저장
 def store_images(g_id):
    total_pics = 900
    hist = get_hand_hist()
@@ -56,23 +58,28 @@ def store_images(g_id):
    frames = 0
    
    while True:
+      ## img 기본 설정 
+      ## thresh(부드러운 흑백 video 화면) 형성
+      ## find countour 
       img = cam.read()[1]
-      img = cv2.flip(img, 1)
+      img = cv2.flip(img, 1) # 카메라 좌우 반전
       imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
       dst = cv2.calcBackProject([imgHSV], [0, 1], hist, [0, 180, 0, 256], 1)
       disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(10,10))
       cv2.filter2D(dst,-1,disc,dst)
+      ## 블러링 과 경계구분(잡음 제거, 부드러운 영상)
       blur = cv2.GaussianBlur(dst, (11,11), 0)
       blur = cv2.medianBlur(blur, 15)
       thresh = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
       thresh = cv2.merge((thresh,thresh,thresh))
       thresh = cv2.cvtColor(thresh, cv2.COLOR_BGR2GRAY)
-      thresh = thresh[y:y+h, x:x+w]
+      thresh = thresh[y:y+h, x:x+w] ## thresh 크기 설정
       contours = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[0]
 
+      ## thresh 화면 캡처후 저장
       if len(contours) > 0:
          contour = max(contours, key = cv2.contourArea)
-         if cv2.contourArea(contour) > 10000 and frames > 50:
+         if cv2.contourArea(contour) > 10000 and frames > 50: ## contour 면적이 일정 기준을 넘어야 캡처 가능
             print("start in contour")
             x1, y1, w1, h1 = cv2.boundingRect(contour)
             pic_no += 1
@@ -82,9 +89,6 @@ def store_images(g_id):
             elif h1 > w1:
                save_img = cv2.copyMakeBorder(save_img, 0, 0, int((h1-w1)/2) , int((h1-w1)/2) , cv2.BORDER_CONSTANT, (0, 0, 0))
             save_img = cv2.resize(save_img, (image_x, image_y))
-            # rand = random.randint(0, 10)
-            # if rand % 2 == 0:
-            #    save_img = cv2.flip(save_img, 1)
             cv2.putText(img, "Capturing...", (30, 60), cv2.FONT_HERSHEY_TRIPLEX, 2, (127, 255, 255))
             cv2.imwrite("gestures/"+str(g_id)+"/"+str(pic_no)+".jpg", save_img)
 
@@ -92,8 +96,11 @@ def store_images(g_id):
       cv2.putText(img, str(pic_no), (30, 400), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (127, 127, 255))
       cv2.imshow("Capturing gesture", img)
       cv2.imshow("thresh", thresh)
+
+
       keypress = cv2.waitKey(1)
-      if keypress == ord('a'):
+      ## c 입력시 캡처 시작
+      if keypress == ord('c'):
          if flag_start_capturing == False:
             flag_start_capturing = True
             print("start capturing flag on")
@@ -101,13 +108,6 @@ def store_images(g_id):
             flag_start_capturing = False
             frames = 0
       if flag_start_capturing == True:
-         # print("frame?+1")
          frames += 1
-      if pic_no == total_pics:
-         break
-
-init_create_folder_database()
-g_id = input("Enter gesture no.: ")
-g_name = input("Enter gesture name/text: ")
-store_in_db(g_id, g_name)
-store_images(g_id)
+      ## 총 캡처수에 도달하면 종료
+      if pic_no == tot
